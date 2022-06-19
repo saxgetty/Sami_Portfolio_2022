66431ccab39ed103302f05a9ce846ac66e22701d
Sami <67216252+saxgetty@users.noreply.github.com>
2022-06-19 16:52:15 -0700

    Skills finished and started on Projects.

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import '../styles/index.css';
+import { Box, Paper, Stack, Grid } from '@mui/material';
+import BusyGator from '../components/project/BusyGator';
+
 
 function Project() {
 
+
     return(
-        <div>
-            <p>Project</p>
-        </div>
+        <>
+            <Grid container
+                // alignItems='center'
+                justifyContent={'center'}
+            > 
+                <Grid 
+                    item xs={12} 
+                    textAlign='center'
+                >
+                    <div class='name'>PROJECTS</div>
+                </Grid>
+                <Grid container> 
+                    <Grid 
+                        item xs={12} sm={12} md={4} 
+                        // textAlign={{ xs: 'center', sm: 'center', md: 'right' }}
+                    >
+                        <BusyGator />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -1,11 +1,123 @@
 import React from 'react';
+import { Stack, Grid, Paper, Box } from '@mui/material/';
 
 function Skill() {
 
     return(
-        <div>
-            <p>Skill</p>
-        </div>
+        <>
+            <Grid container> 
+                <Grid 
+                    item xs={12} 
+                    textAlign='center'
+                >
+                    <div class='name'>SKILLS & TECHNOLOGIES</div>
+                </Grid>
+            </Grid>
+            <Grid container 
+                style={{alignItems: 'center', justifyContent:'center'}}
+            >
+                <Stack container 
+                    direction={{ xs: 'column', sm: 'column', md: 'row', lg: 'row', xl: 'row' }} 
+                    spacing={10}
+                    alignItems={{md: 'center'}}
+                    marginTop={10}
+                >
+                    <Box
+                        sx={{
+                            '& > :not(style)': {
+                                
+                                width: 500,
+                                height: 455,
+                                backgroundColor: '#f2b643',
+                            },
+                        }}
+                    >
+                        <Paper elevation={16}>
+                                <div class='skill_card_title'>
+                                    LANGUAGES
+                                </div>
+                                <div>
+                                    <ul class='list-info'>
+                                        <li>Python</li>
+                                        <li>SQL</li>
+                                        <li>JavaScript</li>
+                                        <li>TypeScript</li>
+                                        <li>Java</li>
+                                        <li>HTML5</li>
+                                        <li>CSS3</li>
+                                        <li>C++</li>
+                                        <li>C</li>
+                                        <li>Swift</li>
+                                    </ul>
+                                </div>
+                        </Paper>
+                    </Box>
+                    <Box
+                        sx={{
+                            '& > :not(style)': {
+                                
+                                width: 500,
+                                height: 455,
+                                backgroundColor: '#f2b643',
+                            },
+                        }}
+                    >
+                        <Paper elevation={16}>
+                            <div class='skill_card_title'>
+                                TECHNOLOGIES
+                            </div>
+                            <div>
+                                <ul class='list-info'>
+                                    <li>Amazon Web Service</li>
+                                    <li>MySQL</li>
+                                </ul>
+                            </div>
+                            <div class='skill_card_title'>
+                                TOOLS
+                            </div>
+                            <div>
+                                <ul class='list-info'>
+                                    <li>Git</li>
+                                    <li>Visual Studio</li>
+                                    <li>Intellij</li>
+                                    <li>Microsoft Office</li>
+                                    <li>VirtualBox</li>
+                                    <li>NetBeans</li>
+                                    <li>Figma</li>    
+                                </ul>
+                            </div>
+                        </Paper>
+                    </Box>
+                    <Box 
+                        sx={{
+                            '& > :not(style)': {
+                                
+                                width: 500,
+                                height: 455,
+                                backgroundColor: '#f2b643',
+                            },
+                        }}
+                    >
+                        <Paper elevation={16}>
+                            <div class='skill_card_title'>
+                                FRAMEWORKS
+                            </div>
+                            <div>
+                                <ul class='list-info'>
+                                    <li>React.js</li>
+                                    <li>Material UI</li>
+                                    <li>Bootstrap</li>
+                                    <li>Node.js</li>
+                                    <li>Express.js</li>
+                                    <li>UIKit</li>
+                                    <li>SwiftUI</li>
+                                </ul>
+                            </div>
+                        </Paper>
+                    </Box>
+                </Stack>
+            </Grid>
+        </>
     );
 }
 
